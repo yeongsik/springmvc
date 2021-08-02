c6ae83bd2d282e2c2b05c0b14a3eb0cfa4477ec7
LAPTOP-N91UPOS7\dudtl <youngsik0266@gmail.com>
2021-08-03 08:04:52 +0900

    Initial commit

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Slf4j
+@RestController
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+//    롬복을 넣으면 자동으로 해줌
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "String";
+
+        // 로그 레벨
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error(" error log={}", name);
+
+        return "ok";
+    }
+}
